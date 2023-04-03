58980d2858140ad129b18d5cc1947a2101c2dc75
Ramakrishna Pillai Aneesh <61421024+aneesh96425234@users.noreply.github.com>
2023-04-03 09:39:55 +0530

    Add files via upload

---
+++ vCenter_Invoke-All_VMs_Script_Victor.ps1
@@ -0,0 +1,18 @@
+﻿$vCenterServer = "aneesh@test.local"
+Connect-VIServer -Server $vCenterServer
+
+$vms = (get-vm) | where {$_.name -like "***vw*" -And $_.name -Notmatch "vct"}).name
+$count = 1
+
+$script =@'
+test-netconnection 192.168.1.105 -port 8530
+test-netconnection 192.168.1.105 -port 8531
+'@
+
+foreach ($vm in $vms)
+
+{ Write-Progress -Id 1 -Activity "invoking ioscan" -Status "current progress: $count of $($vms.count): $vm" -PercentComplete (($count / $vms.count) = 100)
+    Write-Host "checking $vm . . ."
+    Invoke-VMScript -vm $vm -guestuser 'test\aneesh' -guestpassword 'P@ssw0rd' -Scripttype powershell $script
+    $count++
+}
